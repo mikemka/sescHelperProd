1e3f57e56b5fddf45f96b047e6607b42d0749310
микемка <mikemka@vk.com>
2022-09-03 00:36:04 +0500

    hotfix

--- tgbot/handlers/actions.py
+++ tgbot/handlers/actions.py
@@ -10,30 +10,31 @@ import errors
 @dp.message_handler(Timeout(), commands=["start", "reg"])
 async def start(message: types.Message):
     if not BotDB.user_exists(message.from_user.id):
-        return await message.reply(
+        return await message.bot.send_message(
+            message.from_user.id,
             '<b>Приветствуем вас!</b>\n'
             'Представьтесь, пожалуйста. Кто вы?',
             reply_markup=keyboards.StartButtons.keyboard,
-            reply=False,
         )
-    await message.reply(errors.ALREADY_REGISTERED, reply_markup=keyboards.StartButtons1.keyboard, reply=False)
+    await message.bot.send_message(message.from_user.id, errors.ALREADY_REGISTERED, reply_markup=keyboards.StartButtons1.keyboard)
 
 
 @dp.message_handler(Timeout(), commands='cancel')
 async def cancel(message: types.Message):
     user_status.pop(message.from_user.id, None)
-    await message.reply('Вы остановили выполнение операции', reply=False)
+    await message.bot.send_message(message.from_user.id, 'Вы остановили выполнение операции')
 
 
 @dp.message_handler(commands="mi")
 async def timetable_mi(message: types.Message):
-    await message.reply('Выберите день недели', reply_markup=keyboards.all_command_buttons('allmi'), reply=False)
+    await message.bot.send_message(message.from_user.id, 'Выберите день недели', reply_markup=keyboards.all_command_buttons('allmi'))
 
 
 @dp.message_handler(Timeout(), commands="help")
 async def help_command(message: types.Message):
     if BotDB.user_exists(message.from_user.id):
-        return await message.reply(
+        return await message.bot.send_message(
+            message.from_user.id,
             '<b>Помощь по командам</b>\n'
             '\n'
             '/today, /t - Расписание на сегодня\n'
@@ -49,9 +50,8 @@ async def help_command(message: types.Message):
             '/reg - Повторная регистрация\n'
             '/exit - Удалить аккаунт',
             reply_markup=keyboards.keyboard_r(),
-            reply=False,
         )
-    await message.reply(errors.SHOULD_REGISTER, reply_markup=keyboards.StartButton.keyboard, reply=False)
+    await message.bot.send_message(message.from_user.id, errors.SHOULD_REGISTER, reply_markup=keyboards.StartButton.keyboard)
 
 
 @dp.message_handler(Timeout(), commands="status")
@@ -85,9 +85,9 @@ async def lesson_status(message: types.Message):
         return f'{convert_hours(_time // 60)} {convert_minutes(_minutes)}'
     
     if not BotDB.user_exists(message.from_user.id):
-        return await message.reply(errors.SHOULD_REGISTER, reply=False)
+        return await message.bot.send_message(message.from_user.id, errors.SHOULD_REGISTER)
     if datetime.today().weekday() == 6:
-        return await message.reply(errors.NO_LESSONS, reply=False)
+        return await message.bot.send_message(message.from_user.id, errors.NO_LESSONS)
     current_time, e = datetime.today().minute + datetime.today().hour * 60, '<b>Уроки уже закончились</b>'
     if current_time <= 915:
         for k, start_lesson in enumerate((540, 580, 590, 630, 645, 685, 700, 740, 755, 795, 815, 855, 875, 915), start=1):
@@ -95,33 +95,34 @@ async def lesson_status(message: types.Message):
                 e = f"<b>Сейчас идет {k//2} урок</b>\nДо конца урока {convert_time(start_lesson-current_time)}"\
                     if not k % 2 else f'До начала {k // 2 + 1} урока {convert_time(start_lesson-current_time)}'
                 break
-    await message.reply(e, reply_markup=keyboards.CallScheduleButton.keyboard, reply=False)
+    await message.bot.send_message(message.from_user.id, e, reply_markup=keyboards.CallScheduleButton.keyboard)
 
 
 @dp.message_handler(Timeout(), commands=["today", "t"])
 async def today(message: types.Message):
     if BotDB.user_exists(message.from_user.id):
-        return await message.reply(Json.timetable(message.from_user.id, 0), reply=False)
-    await message.reply(errors.SHOULD_REGISTER, reply=False)
+        return await message.bot.send_message(message.from_user.id, Json.timetable(message.from_user.id, 0))
+    await message.bot.send_message(message.from_user.id, errors.SHOULD_REGISTER)
 
 
 @dp.message_handler(Timeout(), commands=["next", "n"])
 async def next_day(message: types.Message):
     if BotDB.user_exists(message.from_user.id):
-        return await message.reply(Json.timetable(message.from_user.id, -1), reply=False)
-    await message.reply(errors.SHOULD_REGISTER, reply=False)
+        return await message.bot.send_message(message.from_user.id, Json.timetable(message.from_user.id, -1))
+    await message.bot.send_message(message.from_user.id, errors.SHOULD_REGISTER)
 
 
 @dp.message_handler(Timeout(), commands=["all", "a"])
 async def all_days(message: types.Message):
     if not BotDB.user_exists(message.from_user.id):
-        return await message.reply(errors.SHOULD_REGISTER, reply=False)
-    await message.reply('Выберите день недели', reply_markup=keyboards.all_command_buttons(), reply=False)
+        return await message.bot.send_message(message.from_user.id, errors.SHOULD_REGISTER)
+    await message.bot.send_message(message.from_user.id, 'Выберите день недели', reply_markup=keyboards.all_command_buttons())
 
 
 @dp.message_handler(Timeout(), commands=["call", "c"])
 async def call_schedule(message: types.Message):
-    return await message.reply(
+    return await message.bot.send_message(
+        message.from_user.id,
         '<b>Расписание звонков</b>\n'
         '\n'
         '<code>1┃ 9:00   9:40</code>\n'
@@ -131,7 +132,6 @@ async def call_schedule(message: types.Message):
         '<code>5┃ 12:35  13:15</code>\n'
         '<code>6┃ 13:35  14:15</code>\n'
         '<code>7┃ 14:35  15:15</code>',
-        reply=False,
     )
 
 
@@ -139,25 +139,25 @@ async def call_schedule(message: types.Message):
 async def unreg(message: types.Message):
     if BotDB.user_exists(message.from_user.id):
         BotDB.remove_user(message.from_user.id)
-        return await message.reply(
+        return await message.bot.send_message(
+            message.from_user.id,
             'Ваш аккаунт был удален! Вы можете зарегистрироваться вновь',
             reply_markup=keyboards.StartButton.keyboard,
-            reply=False,
         )
-    await message.reply(errors.SHOULD_REGISTER, reply_markup=keyboards.StartButton.keyboard, reply=False)
+    await message.bot.send_message(message.from_user.id, errors.SHOULD_REGISTER, reply_markup=keyboards.StartButton.keyboard)
 
 
 @dp.message_handler(commands=["free", "f"])
 async def free_auditories(message: types.Message):
-    await message.reply('Выберите день недели', reply_markup=keyboards.all_command_buttons('allfr'), reply=False)
+    await message.bot.send_message(message.from_user.id, 'Выберите день недели', reply_markup=keyboards.all_command_buttons('allfr'))
 
 
 @dp.message_handler(commands=["th"])
 async def thcom(message: types.Message):
     if BotDB.user_exists(message.from_user.id):
         user_status[message.from_user.id] = 'thcom'
-        return await message.reply('Выберите класс', reply_markup=keyboards.get_forms_keyboard(), reply=False)
-    await message.reply(errors.SHOULD_REGISTER, reply_markup=keyboards.StartButton.keyboard, reply=False)
+        return await message.bot.send_message(message.from_user.id, 'Выберите класс', reply_markup=keyboards.get_forms_keyboard())
+    await message.bot.send_message(message.from_user.id, errors.SHOULD_REGISTER, reply_markup=keyboards.StartButton.keyboard,)
 
 
 @dp.message_handler(IsOwnerFilter(), commands=['mail'])
